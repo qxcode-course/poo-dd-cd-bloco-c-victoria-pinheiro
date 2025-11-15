11cd34dfba2d11331ef3c7dca38e54c345fd5c34
victoria <victoriamariaspinheiro@gmail.com>
2025-11-15 14:47:15 -0300

    estudo.prt1.budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -1,41 +1,27 @@
-class Pessoa:
-    def __init__(self, nome: str):
-        self.nome = nome
-    
-    def __str__(self) -> str:
-        return self.nome
+espera: list[str] = []
 
-    def enter(self, pessoa: Pessoa):  #pessoa indo para a fila
-        self.espera.append(pessoa)
+espera.append("joao")
+espera.append("maria")
+espera.append("bruxa")
 
-    def call(self, index: int):  #chamando a pessoa
-        if index < 0 or index >= len(self.caixa):
-            print("indice inexistente")
-            return
-        if self.caixas[index] is not None:
-            print("Caixa ocupado")
-            return
-        self.caixas[index] = self.espera[0]
-        del self.espera[0]         #deletando a pessoa que ja foi chamada
+del espera[1]
 
+espera = ['maria'] + espera
 
-class Budega:
-    def __init__(self, num_caixas: int):
-        self.caixas: list[Pessoa | None] = []
-        for _ in range(num_caixas):             #laço que vai rodar o tanto de caixas que for inserido
-            self.caixas.append(None)
-        self.espera: list[Pessoa | None] = []
-    
+del espera[0]
 
-    def __str__(self) -> str:
-        self.caixas = [Pessoa("Maria"), None, Pessoa("Joao")]
+espera.append("lobo")
+espera.append("caçador")
 
-        caixas = ",".join([str(x) for x in self.caixas]) #"faça uma ação para cada elemento desse vetor"
-        espera = ",".join([str(x) for x in self.caixas])
+del espera[2]
 
-pessoa = Pessoa("Maria")
-print(pessoa)
+espera.insert(2, "lobo") #insert é para colocar no mesmo lugar de antes (primeiro digita o índice que quer que ele fique e depois o que quer colocar la)
 
-budega = Budega(5)
-budega.caixas[2] = pessoa
-budega.espera.append
+texto = ", ".join(espera)
+
+print(texto)
+
+texto.split("-")
+
+#list comprehension (gera uma lista a partir da outra)
+
